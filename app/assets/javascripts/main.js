ddefeb2d2ceb04eecd564573edf0b202bf630cfc
Dave Woodall <dmwoodall@gmail.com>
2013-04-20 18:37:51 -0600

    something weird happend.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.action-button').mouseenter(function(){
+    $('ul.drawer').show();
+  });
+  $('.action-button').mouseleave(function(){
+    $('ul.drawer').hide();
+  });
+});
+
+
+
+
