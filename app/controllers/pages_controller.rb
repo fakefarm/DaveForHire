98b5c907bd14b9550e6d1147194d2283c0721757
Dave Woodall <dave@hireDave.me>
2013-07-13 09:51:31 -0600

    update actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
   def index;end
-  def experience; end
-  def is_this_you; end
   def services; end
-
-  def art;end
-  def business;end
-  def code;end
-  def tshirts;end
 end
