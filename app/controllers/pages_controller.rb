690df568d681ad4a59f264d4d623e2feeee878ba
Dave Woodall <dave@hireDave.me>
2013-07-13 09:27:03 -0600

    Markup and routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,11 @@
 class PagesController < ApplicationController
-  def index
-  end
+  def index;end
+  def experience; end
+  def is_this_you; end
+  def services; end
 
-  def art
-  end
-
-  def business
-  end
-
-  def code
-  end
-
-  def tshirts
-  end
+  def art;end
+  def business;end
+  def code;end
+  def tshirts;end
 end
